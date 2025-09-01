da181e195d3f5e905a6e6b49e6856c9f8af7ab8c
LucasBonato <lucas.perez.bonato@gmail.com>
2025-08-31 21:40:32 -0300

    [FEAT] Add MongoDB config, MassTransit with outbox pattern and rabbitMQ

---
+++ Infrastructure/MongoDbExtensions.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace F360.JobsProcessor.API.Infrastructure;
+
+public static class MongoDbExtensions {
+	private static IMongoDatabase GetDatabase(this IMongoClient client)
+		=> client.GetDatabase(AppEnv.DATABASE.MONGO.DATABASE_NAME.NotNull());
+
+	private static IMongoCollection<Job> JobsCollection(this IMongoDatabase database)
+		=> database.GetCollection<Job>(AppEnv.DATABASE.MONGO.TABLE.JOBS_COLLECTION_NAME.NotNull());
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,44 @@
+using DotNetEnv;
+using F360.JobsProcessor.API;
+using MassTransit;
+using MongoDB.Driver;
+using Scalar.AspNetCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+Env.TraversePath().Load();
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(AppEnv.DATABASE.MONGO.CONNECTION_URI.NotNull()));
+
+builder.Services.AddMassTransit(configuration => {
+    configuration.AddMongoDbOutbox(config => {
+        config.QueryDelay = TimeSpan.FromSeconds(1);
+
+        config.ClientFactory(provider => provider.GetRequiredService<IMongoClient>());
+        config.DatabaseFactory(provider => provider.GetRequiredService<IMongoDatabase>());
+
+        config.DuplicateDetectionWindow = TimeSpan.FromSeconds(30);
+
+        config.UseBusOutbox();
+    });
+
+    configuration.UsingRabbitMq((context, configurator) => {
+        configurator.Host(new Uri(AppEnv.MESSAGE_BROKER.HOST.NotNull()), host => {
+            host.Username(AppEnv.MESSAGE_BROKER.USERNAME.NotNull());
+            host.Password(AppEnv.MESSAGE_BROKER.PASSWORD.NotNull());
+        });
+
+        configurator.ConfigureEndpoints(context);
+    });
+});
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
